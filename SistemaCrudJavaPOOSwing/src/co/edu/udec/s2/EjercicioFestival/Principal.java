58ce4b2f3dcfb70028e0f042bc6efe21e0fe0e1b
7502410037BillyPeinadoM <bpeinadom@unicartagena.edu.co>
2024-12-12 02:21:05 -0500

    Inferfaz Principal y Inferfaz Artista

--- SistemaCrudJavaPOOSwing/src/co/edu/udec/s2/EjercicioFestival/Principal.java
+++ SistemaCrudJavaPOOSwing/src/co/edu/udec/s2/EjercicioFestival/Principal.java
@@ -1,6 +1,8 @@
 
 package co.edu.udec.s2.EjercicioFestival;
 
+import co.edu.udec.s2.EjercicioFestival.Vistas.GUI.RegistroPrincipal;
+
 /**
  *
  * @author Usuario
@@ -11,7 +13,13 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        RegistroPrincipal registro = new RegistroPrincipal();
+        
+        registro.setExtendedState(RegistroPrincipal.MAXIMIZED_BOTH);
+        
+        registro.setVisible(true);
+        
     }
     
 }
